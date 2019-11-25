f511f6c1028e4c08e9f03592b7cbeba7ec9cebcd
Melissa Holt <melissa.holt21@gmail.com>
2019-11-25 00:57:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,9 @@ class School
   end
   
   def add_student(student, grade)
-   roster[:student] = []
-   roster[:student] << "grade"  
+    roster.each do |student|
+   #roster[:student] = []
+   #roster[:student] << "grade"  
    
    
   end
