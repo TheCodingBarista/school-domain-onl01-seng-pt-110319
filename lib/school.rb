307f62d3d6efeb8546597ee0ce18b8b9a3e91061
Melissa Holt <melissa.holt21@gmail.com>
2019-11-25 01:06:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,20 +10,10 @@ class School
   end
   
   def add_student(student, grade)
-    #self.roster
-   
-    #roster.each do |student|
-      
+  
    roster["student"] = []
    roster["student"] << "grade"  
    
    roster => {:student=>["grade"]}
    
-   
-   hash["new_key"] = []
-hash["new_key"] << "new_value_for_value_array"
- 
-hash
- => {"new_key"=>["new_value_for_value_array"]} 
-  end
 end
